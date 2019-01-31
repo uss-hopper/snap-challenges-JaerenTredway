654095e733676946b4821695de554196d844a179
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-31 08:54:30 -0700

    Added snap-filter.js

---
+++ snap-filter.js
@@ -0,0 +1,36 @@
+//using javascript and the filter array method remove all posts from userId:5 from the array
+
+let testArray = [{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+}, {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+}, {
+    "userId": 3,
+    "id": 30,
+    "title": "a quo magni similique perferendis",
+    "body": "alias dolor cumque\nimpedit blanditiis non eveniet odio maxime\nblanditiis amet eius quis tempora quia autem rem\na provident perspiciatis quia"
+}, {
+    "userId": 4,
+    "id": 31,
+    "title": "ullam ut quidem id aut vel consequuntur",
+    "body": "debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae"
+}, {
+    "userId": 5,
+    "id": 45,
+    "title": "ut numquam possimus omnis eius suscipit laudantium iure",
+    "body": "est natus reiciendis nihil possimus aut provident\nex et dolor\nrepellat pariatur est\nnobis rerum repellendus dolorem autem"
+}, {
+    "userId": 5,
+    "id": 46,
+    "title": "aut quo modi neque nostrum ducimus",
+    "body": "voluptatem quisquam iste\nvoluptatibus natus officiis facilis dolorem\nquis quas ipsam\nvel et voluptatum in aliquid"
+}]
+
+testArray = testArray.filter(element => element.userId !== 5);
+console.log(testArray);
