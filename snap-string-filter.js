0d82b5a46e7bfb3a38aa9bc3e23ab460835d6ef3
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-02-04 08:58:58 -0700

    Added a semicolon.

--- snap-string-filter.js
+++ snap-string-filter.js
@@ -4,7 +4,7 @@
 let array1 = "Fate protects fools, little children and ships named Enterprise. You're going to be an interesting companion, Mr. Data. Some days you get the bear, and some days the bear gets you. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody. Why don't we just give everybody a promotion and call it a night - 'Commander'? Well, that's certainly good to know. Mr. Worf, you do remember how to fire phasers? A lot of things can change in twelve years, Admiral."
 
 function removeF (array) {
-	return array.toLowerCase().split(' ').filter((word) => word.charAt(0) !== 'f')	
+	return array.toLowerCase().split(' ').filter((word) => word.charAt(0) !== 'f');	
 }
 
 console.log(removeF(array1));
