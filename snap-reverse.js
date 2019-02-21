082d1b0309bb82e532ee0e42412ba7b6bcdbab42
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-02-21 08:26:16 -0700

    Removed unnecessary quotes.

--- snap-reverse.js
+++ snap-reverse.js
@@ -5,7 +5,7 @@
 const testString = "I'll alert the crew. We finished our first sensor sweep of the neutral zone. Maybe if we felt any human loss as keenly as we feel one of those close to us, human history would be far less bloody."
 
 function reverseString(string) {
-    return string.split('').reduce((acc, cur) => cur + acc, '');
+    return string.split('').reduce((acc, cur) => cur + acc);
 }
 
 reverseString(testString);
