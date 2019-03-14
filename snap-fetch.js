987460dccff6468e1c3d38c0d5c41f6416470553
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-14 08:57:55 -0600

    Added js functions to fetch and display.

--- snap-fetch.js
+++ snap-fetch.js
@@ -4,10 +4,39 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
-fetch('examples/example.json')
-.then(function(response) {
-  // Do stuff with the response
-})
-.catch(function(error) {
+const pathToResource = "https://jsonplaceholder.typicode.com/posts";
+
+function showJSON(responseAsJSON) {
+  var message = document.getElementById('target');
+  message.textContent = responseAsJSON;
+}
+
+function logResult(result) {
+  console.log(result);
+}
+
+function logError(error) {
   console.log('Looks like there was a problem: \n', error);
-});
+}
+
+function validateResponse(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+function readResponseAsJSON(response) {
+  return response.json();
+}
+
+function fetchJSON(pathToResource) {
+  fetch(pathToResource) // 1
+  .then(validateResponse) // 2
+  .then(readResponseAsJSON) // 3
+  .then(logResult) // 4
+  .then(showJSON(responseAsJSON))
+  .catch(logError);
+}
+
+fetchJSON('/posts');
