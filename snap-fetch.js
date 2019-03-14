f8ad7d3f61e0fae9b743bb4769f9c69df238cc55
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-14 08:24:09 -0600

    Added html file.

--- snap-fetch.js
+++ snap-fetch.js
@@ -1,3 +1,13 @@
-// using fetch display posts fro jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
+// using fetch display posts from jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
+
 // https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+
+fetch('examples/example.json')
+.then(function(response) {
+  // Do stuff with the response
+})
+.catch(function(error) {
+  console.log('Looks like there was a problem: \n', error);
+});
