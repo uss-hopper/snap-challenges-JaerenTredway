1ddd12bf26bddcf951df59fbb7ace6270c8ffd5d
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-03-14 08:16:54 -0600

    Added js file.

---
+++ snap-fetch.js
@@ -0,0 +1,3 @@
+// using fetch display posts fro jsonplaceholder.com onto the dom (https://jsonplaceholder.typicode.com/posts)
+// https://developers.google.com/web/ilt/pwa/working-with-the-fetch-api
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
--- snap-filter-2.js
+++ snap-filter-2.js
@@ -3,7 +3,7 @@
 let testString = "Ensign Babyface! Your shields were failing, sir. This should be interesting. Your head is not an artifact! What's a knock-out like you doing in a computer-generated gin joint like this? Why don't we just give everybody a promotion and call it a night - 'Commander'? But the probability of making a six is no greater than that of rolling a seven. What? We're not at all alike! I recommend you don't fire until you're within 40,000 kilometers. Wouldn't that bring about chaos? Flair is what marks the difference between artistry and mere competence.";
 
 function removeE (string) {
-    return string.toLowerCase().split(' ').filter( word => word.charAt(0) === "e");
+    return string.toLowerCase().split(' ').filter( word => word.charAt(0) !== "e");
 }
 
 console.log(removeE(testString));
